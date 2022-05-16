429aa736f2ef858054429d587caa16d531f1f051
Ingo Janßen <ingo.janssen@brain-maze.de>
2022-05-16 13:08:56 +0200

    Python GUI mit KIVY – Den Benutzer Willkommen heißen

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Ziel ist es Aktien Ticker Symbole in eine Liste eintragen zu können.
 Die Preise werden im Hintergrund abgefragt.
 
 ## Videos
-https://www.youtube.com/watch?v=6XoNpA_oYdw
+- https://www.youtube.com/watch?v=6XoNpA_oYdw
+- https://www.youtube.com/watch?v=lVrfVSBhrcY&feature=emb_imp_woyt
 
 ## Blogposts
 - Python GUI mit KIVY – Erste Schritte mit Fenstern!
+- https://ingo-janssen.de/python-gui-mit-kivy-den-benutzer-willkommen-heissen/
--- kivy_stock_list.py
+++ kivy_stock_list.py
@@ -1,20 +1,49 @@
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 
-# Name vor 'App' wird als Fenster Titel benutzt
 class MyApp(App):
-    # Baut das initiale Fenster auf
     def build(self):
         self.window = GridLayout()
-        # Widgets zum Layout hinzufügen
+        # Setzt das Layout auf eine Spalte - also gesamte Breite
+        self.window.cols = 1
+        # Bild hinzufügen - source ist der PFad zur Datei
+        self.window.add_widget(Image(source='logo_weiß.png'))
+        # Ausgabetext erzeugen
+        self.greeting = Label(text='Wie heißt du?')
+        # Ausgabetext zum Layout hinzufügen
+        self.window.add_widget(self.greeting)
+        # Eingabe vom Benutzer erfragen
+        # multiline=False weil wir nur eine Zeile wollen
+        self.user = TextInput(multiline=False)
+        # Eingabeabfrage zum Layout hinzufügen
+        self.window.add_widget(self.user)
+        # Absenden Button erzeugen
+        self.entrance_button = Button(text='Eintreten')
+        # callback an den Button hinzufügen
+        # Ein callback ist nur eine Referenz auf eine Funktion die
+        # aufgerufen werden soll. Deshalb keine Funktionsklammern '()'
+        self.entrance_button.bind(on_press=self.entrance_button_behaviour)
+        # Button hinzufügen
+        self.window.add_widget(self.entrance_button)
 
         return self.window
 
+    # bei einem 'callback' - also einer Funktion die durch Klick
+    # des Buttons aufgerufen wird, werden mehrere Werte übergeben
+    # Wir interessieren aktuell nicht dafür, deswegen werden sie
+    # einfach in *args gesammelt
+    def entrance_button_behaviour(self, *args):
+        # Label ändern, um den Benutzer zu begrüßen
+        # Dafür nehmen wir den Text aus dem self.user Feld.
+        # Das ist was der Benutzer eingegeben hat
+        self.greeting.text = f'Herzlich Willkommen {self.user.text}.'
 
-# Standard - sollte immer genutzt werden
+
 if __name__ == '__main__':
-    # Instanz der App erzeugen
     app = MyApp()
-    # App starten
     app.run()
